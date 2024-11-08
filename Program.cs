534e8d00c2ce49b1a5fcff2d30621223864286c6
Артем Біленький <artem06bil@gmail.com>
2024-11-08 12:30:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 7;
+            int[,] matrix = new int[size, size];
+            Random random = new Random();
+
+            Console.WriteLine("Початкова матриця:");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    matrix[i, j] = random.Next(0, 101);
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size - i - 1; j++)
+                {
+                    int temp = matrix[i, j];
+                    matrix[i, j] = matrix[size - j - 1, size - i - 1];
+                    matrix[size - j - 1, size - i - 1] = temp;
+                }
+            }
+            Console.WriteLine("Перетворена матриця (симетрична відносно побічної діагоналі):");
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }   
+            Console.ReadKey();
+
+        }
+    }
+}
